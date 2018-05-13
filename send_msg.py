6eb067bff99fd8ea81c16e5e23a71776dd2471e5
Chester <j6fm06503@gmail.com>
2018-05-14 01:30:25 +0800

    Modularization

--- send_msg.py
+++ send_msg.py
@@ -14,6 +14,47 @@ def sendtofb(data): #send json to facebook
         log(r.status_code)
         log(r.text)
 
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"button",
+                    "text": message_text ,
+                    "buttons":[
+                        {
+                            "type":"postback",
+                            "title":"不是我要的答案",
+                            "payload":"不是我要的答案"
+                        }
+                        ]
+                }
+            }
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+def send_template_message(reply):
+    data = json.dumps(reply.template)
+    sendtofb(data)
+
+
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
     sys.stdout.flush()
